0b5244f640e15df08c12277c377134bb877d1624
Alastair Montgomery <amontgomery@williamhill.co.uk>
2018-03-23 14:22:22 +0000

    change image used

--- example.tf
+++ example.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "haggisVM1" {
-  ami           = "ami-0d1102f48ad604cbe"
+  ami           = "ami-084a803c29d3d65c4"
   instance_type = "t2.micro"
   key_name      = "aws_id_rsa.pub"
   security_groups = ["${aws_security_group.allow_ssh.name}","${aws_security_group.allow_https.name}"]
