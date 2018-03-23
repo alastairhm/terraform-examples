b0b8d9d27819ccfd3b172952a84e4e7a7061aec2
Alastair Montgomery <amontgomery@williamhill.co.uk>
2018-03-23 14:16:40 +0000

    using security group name indead of id

--- example.tf
+++ example.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "haggisVM1" {
   ami           = "ami-0d1102f48ad604cbe"
   instance_type = "t2.micro"
   key_name      = "aws_id_rsa.pub"
-#  security_groups = ["${aws_security_group.allow_ssh.id}","${aws_security_group.allow_https.id}"]
+  security_groups = ["${aws_security_group.allow_ssh.name}","${aws_security_group.allow_https.name}"]
   tags {
     Name = "haggisVM1"
   }
